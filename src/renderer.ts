8ea5f54d000b013c79ac25b215a5277913da52e5
Sergey Kazantsev <madgustave@protonmail.ch>
2020-11-05 20:15:59 +0700

    Refactor render functions

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,52 +6,13 @@ import {
   Exporter,
 } from './interfaces';
 
+// TODO: remove render function?
 export async function render(exporter: Exporter, options: RenderOptions) {
-  const browser = await puppeteer.launch({
-    headless: process.env.NODE_ENV === 'chartdev' ? false : true,
-    devtools: process.env.NODE_ENV === 'chartdev' ? true : false,
-    args: [
-      // Required for Docker version of Puppeteer
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      // This will write shared memory files into /tmp instead of /dev/shm,
-      // because Docker’s default for /dev/shm is 64MB
-      '--disable-dev-shm-usage'
-    ],
-  });
-  const page = await browser.newPage();
-
-  // globally init page
-  await page.setViewport({
-    width: options.init.width,
-    height: options.init.height,
-    deviceScaleFactor: 1,
-  });
-
-  const renderIteration = async (chartOptions: ChartOptions) => {
-    try {
-      await exporter.render(page, chartOptions, options.init);
-    } catch (err) {
-      console.error('Error throwed while rendering chart', err);
-    }
-  };
-
-  if (typeof exporter.init === 'function') {
-    await exporter.init(page, options.init);
+  for await (const result of iterableRender(exporter, options)) {
+    // pass
   }
 
-  if (options.charts instanceof Array) {
-    for (const chartOptions of options.charts) {
-      await renderIteration(chartOptions);
-    }
-  }
-  else {
-    return await renderIteration(options.charts);
-  }
-
-  if (process.env.NODE_ENV !== 'chartdev') {
-    await browser.close();
-  }
+  return true;
 }
 
 export async function* iterableRender(exporter: Exporter, options: RenderOptions) {
@@ -94,14 +55,16 @@ export async function* iterableRender(exporter: Exporter, options: RenderOptions
 
   if (options.charts instanceof Array) {
     for (const chartOptions of options.charts) {
-      yield await renderIteration(chartOptions);
+      yield renderIteration(chartOptions);
     }
   }
   else {
-    yield await renderIteration(options.charts);
+    yield renderIteration(options.charts);
   }
 
   if (process.env.NODE_ENV !== 'chartdev') {
     await browser.close();
   }
+
+  return true;
 }
